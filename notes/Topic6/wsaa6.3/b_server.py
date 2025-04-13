bf377c9e94c43f1b9afa08de507ceb77115568d9
Louise Ryan <louisearyan30@gmail.com>
2025-04-13 21:33:09 +0100

    edit

--- notes/Topic6/wsaa6.3/b_server.py
+++ notes/Topic6/wsaa6.3/b_server.py
@@ -1,8 +1,6 @@
 from flask import Flask
 
 # static_url_path is the URL path for static files, static_folder is the folder name for static files
-# static_url_path = '' means the static files will be served from the root URL
-# static_folder = 'staticpages' means the static files are in the 'staticpages' folder
 # This allows you to serve static files (like HTML, CSS, JS) from a specific folder in your project
 # without needing to specify the folder name in the URL.
 
